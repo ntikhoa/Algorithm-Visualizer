be678b1464b55d93ea6289b6eb8e0cfa7b30f4bd
Khoa <ntikhoa123@gmail.com>
2021-08-03 20:17:02 +0700

    drawing columns in order

--- app/src/main/java/com/ntikhoa/algorithmvisualize/CustomView.kt
+++ app/src/main/java/com/ntikhoa/algorithmvisualize/CustomView.kt
@@ -1,10 +1,7 @@
 package com.ntikhoa.algorithmvisualize
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.Nullable
@@ -13,29 +10,47 @@ import kotlin.properties.Delegates
 
 class CustomView(context: Context, @Nullable attrs: AttributeSet) : View(context, attrs) {
 
-    private var rect = Rect()
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private var screenWidth by Delegates.notNull<Int>()
     private var screenHeight by Delegates.notNull<Int>()
 
     private val DEFAULT_POS_X = 0
-    private val DEFAULT_WIDTH = 100
+    private var DEFAULT_WIDTH = 20f
+
+    private val HEIGHT_OFFSET = 50
+
+    private val totalColumn = 20
+
+    private val margin = 10
 
     init {
         screenWidth = context.resources.displayMetrics.widthPixels
         screenHeight = context.resources.displayMetrics.heightPixels
 
-        paint.color = ContextCompat.getColor(getContext(),R.color.teal_700)
+        paint.color = ContextCompat.getColor(context, R.color.teal_700)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        rect.bottom = screenHeight
-        rect.top = rect.bottom - DEFAULT_WIDTH
-        rect.left = DEFAULT_POS_X
+        DEFAULT_WIDTH = (screenWidth - margin * totalColumn) / 20f
+
+        for (i in 0..totalColumn) {
+            drawSquare(canvas, i)
+        }
+    }
+
+    private fun drawSquare(canvas: Canvas?, index: Int) {
+        val rect = RectF()
+        rect.bottom = screenHeight.toFloat()
+        rect.top = rect.bottom - (index + 1) * HEIGHT_OFFSET
+        rect.left = (DEFAULT_POS_X + index * (DEFAULT_WIDTH + margin))
         rect.right = rect.left + DEFAULT_WIDTH
+
+        if (index == 0) {
+            rect.left += margin / 2
+        }
 
         canvas?.drawRect(rect, paint)
     }
